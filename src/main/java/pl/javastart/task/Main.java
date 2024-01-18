515aa4806de77df7872918e39859656a754c5cdf
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-18 15:32:30 +0100

    commit 2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int max = 100;
         System.out.println("Podaj liczbe wierszy.");
         int size1 = scanner.nextInt();
         System.out.println("Podaj liczbe kolumn.");
@@ -13,11 +14,15 @@ public class Main {
         int[][] array = new int[size1][size2];
 
         for (int i = 0; i < array.length; i++) {
+            if (size1 > max || size2 > max) {
+                System.out.println("Podales za duze rozmiary tablicy");
+                break;
+            }
             for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = i * j;
-                System.out.printf("%2d | ", array[i][j]);
+                System.out.printf("%5d | ", array[i][j]);
             }
             System.out.println();
         }
     }
-}
+    }
