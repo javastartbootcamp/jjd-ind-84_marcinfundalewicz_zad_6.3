eb9ce802b616cadb2d22248755eac83d81293031
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-01-18 14:40:07 +0100

    commit 1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,23 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe wierszy.");
+        int size1 = scanner.nextInt();
+        System.out.println("Podaj liczbe kolumn.");
+        int size2 = scanner.nextInt();
+        int[][] array = new int[size1][size2];
 
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = i * j;
+                System.out.printf("%2d | ", array[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
